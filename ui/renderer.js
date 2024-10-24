62a7f526670e1277479b770f4ef02feef5182ea4
bhagat-abhishek <inst.abhishek@gmail.com>
2024-10-24 14:30:33 +0530

    setup electron app.

---
+++ ui/renderer.js
@@ -0,0 +1,2 @@
+information = document.getElementById("info");
+information.innerText = `You are using Chrome (v${versions.chrome()}), Node (v${versions.node()}) and Electron (v${versions.electron()}). Enjoy`;
